78f20d89588a608d4aecd9a40c0258714c1e21eb
sdmoran <sdhm87@gmail.com>
2019-09-26 22:17:58 +0000

    Figured out git weirdness with previously tracked files now in gitignore

--- server/permissions.js
+++
@@ -1,9 +0,0 @@
-const database = {
-        host     : 'localhost',
-        user     : 'root',
-        password : 'password',
-        database : 'testschema',
-        connectTimeout: 10000
-    }
-
-exports.database = database;
