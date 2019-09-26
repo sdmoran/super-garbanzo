362cce6cc671b43bd191ba538ad019b6f61edc80
sdmoran <sdhm87@gmail.com>
2019-09-26 22:12:23 +0000

    Revert "Thank you VSCode for committing files in the .gitignore. Fixed I hope"
    This reverts commit 4af54893104a7940b9255eec31aec68caca342bf.

---
+++ server/permissions.js
@@ -0,0 +1,9 @@
+const database = {
+        host     : 'localhost',
+        user     : 'root',
+        password : 'password',
+        database : 'testschema',
+        connectTimeout: 10000
+    }
+
+exports.database = database;
