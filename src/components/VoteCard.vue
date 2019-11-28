89a429db6f4926900c9bc64733069829380069d5
sdmoran <smoran@wpi.edu>
2019-11-27 22:20:18 -0500

    Remove question id from vote card display

--- src/components/VoteCard.vue
+++ src/components/VoteCard.vue
@@ -3,7 +3,6 @@
         <div class="card mx-auto" style="width: 20rem;">
             <div class="card-body">
                 <h5 class="card-title">{{this.text}}</h5>
-                <p>{{this.id}}</p>
                 <button type="submit" v-on:click="sendVote()">Vote</button>
             </div>
         </div>
